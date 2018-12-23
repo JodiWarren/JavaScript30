583cfac2f7904e6c2681e7f3bb3eb741e6723bee
Jodi Warren <jodi@jodiwarren.com>
2018-12-23 23:00:14 +0000

    Build JS for 02-Clock

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,22 @@
+var hourHand = document.querySelector('.hour-hand');
+var minuteHand = document.querySelector('.min-hand');
+var secondHand = document.querySelector('.second-hand');
+function getHandPosition(currentTime, totalTime) {
+    var percentage = currentTime / totalTime;
+    return (360 * percentage) + 90;
+}
+function tick() {
+    var time = new Date;
+    var hour = time.getHours() > 11 ? time.getHours() - 12 : time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    if (!(hourHand instanceof HTMLElement) ||
+        !(minuteHand instanceof HTMLElement) ||
+        !(secondHand instanceof HTMLElement)) {
+        throw new Error("element #test not in document");
+    }
+    hourHand.style.transform = "rotateZ( " + getHandPosition(hour, 12) + "deg)";
+    minuteHand.style.transform = "rotateZ( " + getHandPosition(minutes, 60) + "deg)";
+    secondHand.style.transform = "rotateZ( " + getHandPosition(seconds, 60) + "deg)";
+}
+window.setInterval(tick, 1000);
---
+++ 02 - JS and CSS Clock/script.ts
@@ -0,0 +1,30 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function getHandPosition(currentTime, totalTime) {
+    const percentage = currentTime / totalTime;
+    return (360 * percentage) + 90;
+}
+
+function tick() {
+    const time = new Date;
+    const hour = time.getHours() > 11 ? time.getHours() - 12 : time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    if (
+        !(hourHand instanceof HTMLElement) ||
+        !(minuteHand instanceof HTMLElement) ||
+        !(secondHand instanceof HTMLElement)
+    ) {
+        throw new Error("element #test not in document")
+    }
+
+    hourHand.style.transform = `rotateZ( ${getHandPosition(hour, 12)}deg)`;
+    minuteHand.style.transform = `rotateZ( ${getHandPosition(minutes, 60)}deg)`;
+    secondHand.style.transform = `rotateZ( ${getHandPosition(seconds, 60)}deg)`;
+
+}
+
+window.setInterval(tick, 1000);
